5350842ced63484b6c475e55fd11dbfc86910dcd
Lucas Machain <muyimacha@gmail.com>
2022-03-03 21:41:18 -0300

    lasts uploads

---
+++ js/export.js
@@ -0,0 +1,13 @@
+const cities = {
+	"Shenzhen";
+	"Beijing";
+	"Shanghai";
+	"Guangzhou"
+};
+
+module.exports = {
+	cities: cities
+};
+
+// Shift + V -> VISUAL LINE MODE
+// In VISUAL LINE Shift + <, > lets you indent multiple lines at the same time
